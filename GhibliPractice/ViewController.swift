332e032cf3224256675421845fbea76bf96ce515
Iram Fattah <iramfattah94@gmail.com>
2019-10-01 18:37:16 -0400

    added xcode project and image assets

---
+++ GhibliPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GhibliPractice
+//
+//  Created by C4Q on 10/1/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
